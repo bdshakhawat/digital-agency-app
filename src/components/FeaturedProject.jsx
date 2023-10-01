2d372e482c8c77de69fbb512f4863ce199d92764
bdshakhawat <mdshakwat.hossain@gmail.com>
2023-10-01 21:50:22 +0600

    fix commit

--- src/components/FeaturedProject.jsx
+++ src/components/FeaturedProject.jsx
@@ -1,71 +1,79 @@
-import Image from "next/image";
-async function getData() {
-  try {
-    const response = await fetch('https://agency.teamrabbil.com/api/FeaturedProject');
-    if (!response.ok) {
-      throw new Error('All project calling failed');
+import Link from "next/link";
+
+async function getData(){
+    const res=await fetch("https://agency.teamrabbil.com/api/FeaturedProject");
+    if(!res.ok){
+        throw new Error("FeaturedProject Calling Fail");
     }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
+    return res.json();
 }
 
+
 const FeaturedProject = async () => {
-  const data = await getData();
-  return (
-    <>
-      <section>
-        <div className="py-20 bg-gray-50 radius-for-skewed">
-          <div className="container mx-auto px-4">
-            <div className="flex flex-wrap items-center -mx-4">
 
-              <div className="mb-12 lg:mb-0 w-full lg:w-1/2 flex px-4">
-                <div className="max-w-md">
-                  <span className="text-green-600 font-bold">
-                    Dolor sit amet consectutar
-                  </span>
-                  <h2 className="mb-6 text-4xl lg:text-5xl font-bold font-heading">
-                    Build &amp; Launch without problems
-                  </h2>
-                  <div className="mb-6 max-w-sm">
-                    <p className="text-gray-500 leading-loose">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Pellentesque efficitur nisl sodales egestas lobortis.
-                    </p>
-                  </div>
-                  <div className="flex flex-wrap lg:-ml-5">
-                    <button className="lg:w-auto py-2 px-6 leading-loose lg:ml-5 text-gray-50 font-bold bg-green-600 hover:bg-green-700 transition duration-200 rounded-l-xl rounded-t-xl">
-                      Get Started
-                    </button>
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-wrap w-full lg:w-1/2">
-                   {data.map((item, i) => (                
-                      <div className=" lg:mb-2 w-full  lg:w-1/2 px-3">
+    const data = await getData();
 
-           
-                        <img key={i}
-                        
-                          className="w-full h-32 lg:h-48 object-cover rounded"
-                          src={item["image"]}
-                          alt=""
-                        />
-                      </div>
-                    ))}
- 
-
-
-              </div>
+    return (
+        <section>
+            <div className="skew skew-top mr-for-radius">
+                <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-50" viewBox="0 0 10 10" preserveAspectRatio="none">
+                    <polygon fill="currentColor" points="0 0 10 10 0 10"/>
+                </svg>
             </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+            <div className="skew skew-top ml-for-radius">
+                <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-50" viewBox="0 0 10 10" preserveAspectRatio="none">
+                    <polygon fill="currentColor" points="0 10 10 0 10 10"/>
+                </svg>
+            </div>
+            <div className="py-20 bg-gray-50 radius-for-skewed">
+                <div className="container px-4 mx-auto">
+                    <div className="mb-16 flex flex-wrap justify-center md:justify-between items-center">
+                        <div className="text-center lg:text-left">
+                            <span className="text-green-600 font-bold">
+                            Dolor sit amet consectutar
+                            </span>
+                            <h2 className="text-4xl lg:text-5xl font-bold font-heading">
+                                Featured Projects
+                            </h2>
+                        </div>
+                        <Link className="hidden md:inline-block py-2 px-6 rounded-l-xl rounded-t-xl bg-green-600 hover:bg-green-700 text-gray-50 font-bold leading-loose transition duration-200" href="/projects">View More Projects</Link>
+                    </div>
+                    <div className="flex flex-wrap -mx-4 mb-4">
+                        {
+                            data.map((item,i)=>{
+                                return(
+                                    <div key={i} className="w-full md:w-1/2 lg:w-1/3 mb-8 px-4">
+                                        <a target="_blank" href={item['live']}>
+                                            <img className="h-80 w-full mx-auto object-cover rounded" src={item['image']} alt=""/>
+                                        </a>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="text-center">
+                        <Link className="md:hidden inline-block py-2 px-6 rounded-l-xl rounded-t-xl bg-green-600 hover:bg-green-700 text-gray-50 font-bold leading-loose transition duration-200" href="/projects">
+                            View More Projects
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="skew skew-bottom mr-for-radius">
+                <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-50" viewBox="0 0 10 10" preserveAspectRatio="none">
+                    <polygon fill="currentColor" points="0 0 10 0 0 10"/>
+                </svg>
+            </div>
+            <div className="skew skew-bottom ml-for-radius">
+                <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-50" viewBox="0 0 10 10" preserveAspectRatio="none">
+                    <polygon fill="currentColor" points="0 0 10 0 10 10"/>
+                </svg>
+            </div>
+        </section>
+    );
 };
 
 export default FeaturedProject;
+
+
+
+
